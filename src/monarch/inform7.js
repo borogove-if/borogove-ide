1014e9063700cc5e5b4518f87fc364c5453f9ad6
Juhana Leinonen <git@nitku.net>
2020-07-26 21:00:44 +0300

    webpack-dev-server config update

--- src/monarch/inform7.js
+++ src/monarch/inform7.js
@@ -1,44 +1,44 @@
 export default {
-  ignoreCase: true,
-  brackets: [],
+    ignoreCase: true,
+    brackets: [],
 
-  tokenizer: {
-    root: [
-      // Headers
-      [/^\s*(volume|book|part|chapter|section)\s.*$/, 'type'],
+    tokenizer: {
+        root: [
+            // Headers
+            [ /^\s*(volume|book|part|chapter|section)\s.*$/, "type" ],
 
-      // Strings
-      [/"/, 'string', '@string'],
+            // Strings
+            [ /"/, "string", "@string" ],
 
-      // Comments
-      [/\[/, 'comment.doc', '@comment'],
-      
-      // I6 inclusions
-      [/^\(\-/, { token: 'delimiter', bracket: '@open', next: '@inform6', nextEmbedded: 'inform6' }],
-      [/^-\)/, { token: 'delimiter', bracket: '@close' } ],
+            // Comments
+            [ /\[/, "comment.doc", "@comment" ],
 
-      // Extension documentation
-      [/^\s*----\s+DOCUMENTATION\s+----/, 'comment.doc', '@extension_doc']
-    ],
+            // I6 inclusions
+            [ /^\(-/, { token: "delimiter", bracket: "@open", next: "@inform6", nextEmbedded: "inform6" } ],
+            [ /^-\)/, { token: "delimiter", bracket: "@close" } ],
 
-    comment: [
-      [/[^\]\[]+/, 'comment'],
-			[/\]/, 'comment', '@pop'],
-			[/\[/, 'comment', '@push']
-    ],
+            // Extension documentation
+            [ /^\s*----\s+DOCUMENTATION\s+----/, "comment.doc", "@extension_doc" ]
+        ],
 
-    string: [
-      [/\[.*?\]/, 'tag'],
-      [/[^"]/, 'string'],
-      [/"/, 'string', '@pop']
-    ],
+        comment: [
+            [ /[^\][]+/, "comment" ],
+            [ /\]/, "comment", "@pop" ],
+            [ /\[/, "comment", "@push" ]
+        ],
 
-    inform6: [
-      [/^-\)/, { token: '@rematch', next: '@pop', nextEmbedded: '@pop' }]
-    ],
+        string: [
+            [ /\[.*?\]/, "tag" ],
+            [ /[^"]/, "string" ],
+            [ /"/, "string", "@pop" ]
+        ],
 
-    extension_doc: [
-      [/.*/, 'comment.doc']
-    ]
-  },
+        inform6: [
+            [ /^-\)/, { token: "@rematch", next: "@pop", nextEmbedded: "@pop" } ]
+        ],
+
+        extension_doc: [
+            [ /.*/, "comment.doc" ]
+        ]
+    }
 };
