1e2b99a4bb6bd16edb3b3cdbec26abcc9eb71a52
Juhana Leinonen <git@nitku.net>
2020-10-11 18:16:21 +0300

    Snippets project build rules

--- config/paths.js
+++ config/paths.js
@@ -52,7 +52,7 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('build'),
+  appBuild: resolveApp( 'build-' + process.env.REACT_APP_VARIANT ),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
