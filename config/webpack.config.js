97d6de25c33c7b07331c171bfeda2465bd987401
Juhana Leinonen <git@nitku.net>
2021-04-26 23:27:00 +0300

    Re-added text file imports

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -368,6 +368,11 @@ module.exports = function (webpackEnv) {
           // match the requirements. When no loader matches it will fall
           // back to the "file" loader at the end of the loader list.
           oneOf: [
+            // Import plain text files as strings
+            {
+              test: /\.txt$/i,
+              loader: require.resolve('raw-loader')
+            },
             // TODO: Merge this config once `image/avif` is in the mime-db
             // https://github.com/jshttp/mime-db
             {
