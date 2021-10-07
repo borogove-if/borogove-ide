97832e4f301c73eab112a0c697bd9defb70414e2
Juhana Leinonen <git@nitku.net>
2021-10-07 20:57:04 +0300

    Fixed Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+    typescript: {
+        // Must disable this because it throws an error in latest Typescript,
+        // can try to remove this line later when the libraries have been updated
+        reactDocgen: 'none'
+    }
+};
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,17 +3,6 @@ const paths = require('../config/paths');
 
 module.exports = ({ config }) => {
   config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve('awesome-typescript-loader')
-      },
-      {
-        loader: require.resolve('react-docgen-typescript-loader')
-      }
-    ]
-  });
-  config.module.rules.push({
     test: /\.(scss|sass)$/,
     exclude: /\.module\.(scss|sass)$/,
     use: [
