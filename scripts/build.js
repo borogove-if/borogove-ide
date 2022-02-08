e822ea2d18a4b98c1c5bdfcd2c959e0da3fd217e
Juhana Leinonen <git@nitku.net>
2022-02-08 19:30:04 +0200

    Cross-platform way to set the variant env variable. Closes #6

--- scripts/build.js
+++ scripts/build.js
@@ -4,6 +4,9 @@
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 
+// Set the variant environment variable from the parameter.
+process.env.REACT_APP_VARIANT = process.argv[2];
+
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
